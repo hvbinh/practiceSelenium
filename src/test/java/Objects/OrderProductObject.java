72e14fb7a2e333dbc8b55793bae9b945c8043527
hvbinh <havanbinhk13t01@gmail.com>
2020-05-06 00:00:59 +0700

    in progress order product

--- src/test/java/Objects/OrderProductObject.java
+++ src/test/java/Objects/OrderProductObject.java
@@ -2,12 +2,15 @@ package Objects;
 
 import Supports.Browsers;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.LoadableComponent;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -30,17 +33,16 @@ public class OrderProductObject extends LoadableComponent<OrderProductObject> {
 
     }
     //expose element
-    @FindBy(xpath = "//a[@title='Add to cart']/span[.='Add to cart']")
-    WebElement addToCart_button;
+    @FindBy(xpath = "//a[@title='Proceed to checkout']")
+    WebElement proceedToCheckOut1_button;
+    @FindBy(xpath = "//p[@class='cart_navigation clearfix']/a[@title='Proceed to checkout']")
+    WebElement proceedToCheckOut2_button;
+    public WebElement productName(String name)
+    {
+       return Browsers.getDriver().findElement(By.xpath(String.format("//a[@title='%s']", name)));
+    }
 
-    public WebElement productName(WebElement e, String nameProduct)
-    {
-        return e.findElement(By.xpath(String.format("//div/div/div/a[@title='%s']",nameProduct)));
-    }
-    public WebElement addToCart(WebElement e, String nameProduct)
-    {
-        return e.findElement(By.xpath(String.format("//div/div/div/a[@title='%s']/span[.='Add to cart']",nameProduct)));
-    }
+
     public List<WebElement> productList()
     {
         return  Browsers.getDriver().findElements(By.xpath("//div[@class='tab-content']/ul[@id='homefeatured']/li"));
@@ -53,24 +55,16 @@ public class OrderProductObject extends LoadableComponent<OrderProductObject> {
     public void orderProduct(String name)
     {
         Actions act = new Actions(Browsers.getDriver());
-        List<WebElement> list = productList();
-        System.out.println("size: "+list.size());
-        for(WebElement e: list)
-        {
-            System.out.println("size: "+e.getAttribute("class"));
-            WebElement product = productName(e,name);
-            if(product.getAttribute("title").equalsIgnoreCase("Blouse"))
-            {
-                act.moveToElement(product).click(product.findElement(By.xpath("//span[contains(text(),'Add to cart')]"))).perform();
-                //Browsers.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-                break;
-
-            }
-
-
-
-        }
-
+        WebElement e = productName("Blouse");
+        act.moveToElement(e).perform();
+        e.findElement(By.xpath("//a[@title='Blouse']/../../../..//a[@class='button ajax_add_to_cart_button btn btn-default']")).click();
+        WebDriverWait wait = new WebDriverWait(Browsers.getDriver(), 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@title='Proceed to checkout']")));
+        proceedToCheckOut1_button.click();
+       /* JavascriptExecutor js = (JavascriptExecutor) Browsers.getDriver();
+        js.executeScript("window.scrollBy(0,1000)");*/
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@class='cart_navigation clearfix']/a[@title='Proceed to checkout']")));
+        proceedToCheckOut2_button.click();
 
 
     }
